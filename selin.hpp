22a9d59e169e802b170aa4f499946a69d4ea06ef
egorcikselugin-arch <egorcikselugin@gmail.com>
2026-09-08 13:26:31 +0300

    Fix code errors and update project structure

--- selin.hpp
+++ selin.hpp
@@ -1,13 +1,15 @@
+#pragma once
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Hello World!";
+// Переносим код логики внутрь функции print
+void print() {
+    cout << "Hello World!" << endl;
     int x;
-    cout << "input x:";
+    cout << "input x: ";
     cin >> x;
-    if (x!=0) {
-        cout <<100/x;
+    if (x != 0) {
+        cout << 100.0 / x << endl; // используем 100.0 для корректного деления дробных чисел
     }
-    return 0;
 }
