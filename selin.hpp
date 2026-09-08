fddea66727949348f2525a54c69e74264c5d5f14
egorcikselugin-arch <egorcikselugin@gmail.com>
2026-09-08 13:32:24 +0300

    Fix syntax error in selin.hpp

--- selin.hpp
+++ selin.hpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-// Переносим код логики внутрь функции print
 void print() {
     cout << "Hello World!" << endl;
     int x;
     cout << "input x: ";
     cin >> x;
     if (x != 0) {
-        cout << 100.0 / x << endl; // используем 100.0 для корректного деления дробных чисел
+        cout << 100.0 / x << endl;
     }
 }
